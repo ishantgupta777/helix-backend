115c505cd9f0e7c49f1a7d6804b5d020b39da1c3
Ishantgupta777 <Ishantgupta777@gmail.com>
2020-06-14 08:45:47 +0530

    config refactored

--- config/db.config.js
+++ config/db.config.js
@@ -1,13 +1,41 @@
-module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "httest_live_stream",
-  dialect: "mysql",
+const Sequelize = require('sequelize');
+
+const dbConfig = {
+  HOST: 'localhost',
+  USER: 'root',
+  PASSWORD: '',
+  DB: 'httest_live_stream',
+  dialect: 'mysql',
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000
-  }
+    idle: 10000,
+  },
 };
+
+if (process.env.NODE_ENV !== 'production') {
+  const sequelize = new Sequelize(
+    dbConfig.DB,
+    dbConfig.USER,
+    dbConfig.PASSWORD,
+    {
+      host: dbConfig.HOST,
+      dialect: dbConfig.dialect,
+      operatorsAliases: 0,
+
+      pool: {
+        max: dbConfig.pool.max,
+        min: dbConfig.pool.min,
+        acquire: dbConfig.pool.acquire,
+        idle: dbConfig.pool.idle,
+      },
+    }
+  );
+  module.exports = sequelize;
+} else {
+  const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
+    dialect: dbConfig.dialect,
+  });
+  module.exports = sequelize;
+}
--- models/index.js
+++ models/index.js
@@ -1,24 +1,12 @@
-const dbConfig = require("../config/db.config.js");
+const Sequelize = require('sequelize');
 
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorsAliases: 0,
-
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  }
-});
+const sequelize = require('../config/db.config');
 
 const db = {};
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.users = require("./User")(sequelize, Sequelize);
+db.users = require('./User')(sequelize, Sequelize);
 
 module.exports = db;
